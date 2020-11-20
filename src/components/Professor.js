936bc038ba461c353b73069e6a7b1c1bfc78cb2e
Renan Barbosa <renan_barbosa91@hotmail.com>
2020-11-20 18:26:42 -0300

    tabela adicionada

--- src/components/Professor.js
+++ src/components/Professor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import requisicao from '../Api'
 import '../style/Geral.css'
+import { Table } from 'reactstrap';
 
 export default class Professor extends Component {
     state = {
@@ -17,7 +18,19 @@ export default class Professor extends Component {
         const { dados } = this.state;
         return (
             <div id="lista">
-                {dados.map(usr => (
+                <Table dark striped hover responsive>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Nome</th>
+                                <th>E-Mail</th>
+                                <th>R.A</th>
+                                <th>Senha</th>
+                            </tr>
+                        </thead>
+
+                    {dados.map(usr => (
+                    /*
                     <span id={usr.Id} key={usr.Id}>
                         <div className="nome">Nome: {usr.Nome}</div>
                         <div className="email">E-Mail: {usr.Email}</div>
@@ -25,11 +38,21 @@ export default class Professor extends Component {
                         <div className="ra">R.A: {usr.Ra}</div>
                         <div className="senha">Senha: {usr.Senha}</div>
                         <hr></hr>
-                    </span>
-                )
-                )}
+                    */
+                    <tbody>
+                        <tr id={usr.Id} key={usr.Id}>
+                            <th scope="row">{usr.Id}</th>
+                            <td className="nome">{usr.Nome}</td>
+                            <td className="email">{usr.Email}</td>
+                            <td className="ra">{usr.Ra}</td>
+                            <td className="senha">{usr.Senha}</td>
+                        </tr>
+                    </tbody>
+                    //</span>
+                    ))}
+                </Table>
             </div>
-            
+
         );
     }
 
