30a946335a70672bbed9b504135982bdfa94f7bc
Renan Barbosa <renan_barbosa91@hotmail.com>
2020-11-20 18:31:58 -0300

    fix

--- src/components/Professor.js
+++ src/components/Professor.js
@@ -8,7 +8,6 @@ export default class Professor extends Component {
         dados: []
     }
 
-    //chamado imediatamente após alguma atualização ocorrer
     async componentDidMount() {
         const retorno = await requisicao.get('professores');
         this.setState({ dados: retorno.data });
@@ -19,40 +18,29 @@ export default class Professor extends Component {
         return (
             <div id="lista">
                 <Table dark striped hover responsive>
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Nome</th>
-                                <th>E-Mail</th>
-                                <th>R.A</th>
-                                <th>Senha</th>
-                            </tr>
-                        </thead>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nome</th>
+                            <th>E-Mail</th>
+                            <th>R.A</th>
+                            <th>Senha</th>
+                        </tr>
+                    </thead>
 
                     {dados.map(usr => (
-                    /*
-                    <span id={usr.Id} key={usr.Id}>
-                        <div className="nome">Nome: {usr.Nome}</div>
-                        <div className="email">E-Mail: {usr.Email}</div>
-                        <div className="nascimento">Nascimento: {usr.Nascimento}</div>
-                        <div className="ra">R.A: {usr.Ra}</div>
-                        <div className="senha">Senha: {usr.Senha}</div>
-                        <hr></hr>
-                    */
-                    <tbody>
-                        <tr id={usr.Id} key={usr.Id}>
-                            <th scope="row">{usr.Id}</th>
-                            <td className="nome">{usr.Nome}</td>
-                            <td className="email">{usr.Email}</td>
-                            <td className="ra">{usr.Ra}</td>
-                            <td className="senha">{usr.Senha}</td>
-                        </tr>
-                    </tbody>
-                    //</span>
+                        <tbody>
+                            <tr id={usr.Id} key={usr.Id}>
+                                <th scope="row">{usr.Id}</th>
+                                <td className="nome">{usr.Nome}</td>
+                                <td className="email">{usr.Email}</td>
+                                <td className="ra">{usr.Ra}</td>
+                                <td className="senha">{usr.Senha}</td>
+                            </tr>
+                        </tbody>
                     ))}
                 </Table>
             </div>
-
         );
     }
 
